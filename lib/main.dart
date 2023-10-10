031eb5af1f299651be20657f3dce09e337dfda4f
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2023-10-10 22:32:47 +0600

    Remane HomeScreen File to SigninScreen File

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/views/homescreen.dart';
+import 'package:note_app/views/singinscreen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
