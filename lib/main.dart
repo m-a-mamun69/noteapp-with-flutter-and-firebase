3c32d5bab47bba95300cdeec899c0a292b86051a
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2023-10-10 19:46:42 +0600

    Another Commit After Creating Login Screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
--- lib/views/homescreen.dart
+++ lib/views/homescreen.dart
@@ -1,19 +1,89 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sized_box_for_whitespace
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("Note App"),
+        title: const Text("Login Screen"),
+        // actions: [Icon(Icons.more_vert)],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                height: 300.0,
+                child: Lottie.asset("assets/animation_lnkbeh11.json"),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    enabledBorder: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    enabledBorder: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.password),
+                    suffixIcon: Icon(Icons.visibility),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Login"),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("Forgot Password"),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("Don't have an account SingUp"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
